7eb2d9f0aaf4a479441ef07dd408385acbd08bfe
Daryl Pinto <daryl.j.pinto@gmail.com>
2018-05-16 10:21:17 -0400

    Code quality improvements based on feedback

--- src/components/js/App.js
+++ src/components/js/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
-import TodoList from './TodoList';
-import ViewToggle from './ViewToggle';
+import '../css/App.css';
 
+//Constants
 const ENTER_KEY = 13;
+const VIEW_ALL = "all";
+const VIEW_ACTIVE = "active";
+const VIEW_COMPLETED = "completed";
 
+//Utilities
+const clone = obj => JSON.parse(JSON.stringify(obj));
+
+//Stateless Functional Components
+const TodoList = props => {
+	const {todos, onTodoClick} = props;
+	return (
+		<ul>
+			{todos.map(todo => (
+				<li key={todo.id} onClick={() => onTodoClick(todo.id)} className={todo.completed ? "completed" : null}>
+					{todo.text}
+				</li>
+			))}
+		</ul>
+	);
+};
+
+const ViewToggle = props => {
+	const {onViewChange} = props;
+	return (
+		<div id="view-toggle">	
+			<label htmlFor="view-all">All</label>
+			<input type="radio" id="view-all" name="currentView" value={VIEW_ALL} onChange={onViewChange} defaultChecked="checked" />
+
+			<label htmlFor="view-active">Active</label>
+			<input type="radio" id="view-active" name="currentView" value={VIEW_ACTIVE} onChange={onViewChange} />
+
+			<label htmlFor="view-completed">Completed</label>
+			<input type="radio" id="view-completed" name="currentView" value={VIEW_COMPLETED} onChange={onViewChange} />
+		</div>
+	);
+};
+
+//Main Component
 class App extends Component {
 
 	constructor(props){
@@ -16,16 +53,17 @@ class App extends Component {
 	}
 
 	//Input Handlers
-	handleChange = e => this.setState({newTodo: e.target.value});
+	handleChange = e => this.setState({newTodo: e.target.value})
 
 	handleKeyDown = e => {
-		if(e.keyCode !== ENTER_KEY) return;
+		const todoText = this.state.newTodo.trim();
+		if(e.keyCode !== ENTER_KEY || todoText.length === 0) return;
 
 		this.setState(prevState => ({
 			newTodo: "",
 			todos: [...prevState.todos, {
 				id: Date.now(),
-				text: prevState.newTodo,
+				text: todoText,
 				completed: false
 			}]
 		}));
@@ -33,17 +71,17 @@ class App extends Component {
 
 	//To-do List Handlers
 	toggleTodoComplete = id => {
-		let todos = [...this.state.todos];
-		let target = todos.find(todo => todo.id === id);
-		target.completed = !target.completed;
+		const todos = clone(this.state.todos);
+		let todo = todos.find(todo => todo.id === id);
+		todo.completed = !todo.completed;
 		this.setState({todos});
 	}
 
 	//Control Panel Handlers
-	updateCurrentView = e => this.setState({currentView: e.target.value});
+	updateCurrentView = e => this.setState({currentView: e.target.value})
 
 	completeAll = () => {
-		let todos = [...this.state.todos];
+		const todos = clone(this.state.todos);
 		todos.every(t => t.completed) ?
 			todos.forEach(t => t.completed = false) :
 			todos.forEach(t => t.completed = true);	
@@ -51,26 +89,37 @@ class App extends Component {
 	}
 
 	clearCompleted = () => {
-		let todos = [...this.state.todos].filter(todo => !todo.completed);
+		let todos = clone(this.state.todos).filter(todo => !todo.completed);
 		this.setState({todos});
 	}
 
 	render() {
-		const todos = [...this.state.todos];
-		let shownTodos = todos;
 
-		if(this.state.currentView === "active") shownTodos = todos.filter(todo => !todo.completed);
-		else if(this.state.currentView === "completed") shownTodos = todos.filter(todo => todo.completed);
+		const {newTodo, currentView, todos} = this.state;
+		let shownTodos;
+
+		switch(currentView){
+			case VIEW_ACTIVE:
+				shownTodos = todos.filter(todo => !todo.completed);
+				break;
+			case VIEW_COMPLETED:
+				shownTodos = todos.filter(todo => todo.completed);
+				break;
+			default:
+				shownTodos = todos;
+		}
 	
 		return (
 			<main>
 				<h1>To-do List</h1>
-				<input placeholder="What needs to be done?" autoFocus value={this.state.newTodo} onChange={this.handleChange} onKeyDown={this.handleKeyDown} />
+				<input placeholder="What needs to be done?" autoFocus value={newTodo} onChange={this.handleChange} onKeyDown={this.handleKeyDown} />
 				<TodoList todos={shownTodos} onTodoClick={this.toggleTodoComplete} />
 				<div className="control-panel">
 					<ViewToggle onViewChange={this.updateCurrentView} />
-					<button onClick={this.completeAll}>Complete All</button>
-					<button onClick={this.clearCompleted}>Clear Completed</button>
+					<button type="button" onClick={this.completeAll}>Complete All</button>
+					{todos.some(todo => todo.completed) &&
+						<button type="button" onClick={this.clearCompleted}>Clear Completed</button>
+					}	
 				</div>
 			</main>
 		);
